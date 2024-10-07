8d2078c2ad4a089102fe3113a0b6d44aed130ffc
msiwy12345 <mateuszsiwy00@gmail.com>
2024-10-07 15:02:05 +0200

    Updated the Projects and Skills section

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -7,26 +7,21 @@ function Projects() {
         <h1 className={styles.header}>Projects</h1>
         <h2>Console Apps</h2>
         <div className={styles.projects}>
-            <ProjectCard src={black} link='' h3='Project' p='Description'/>
-            <ProjectCard src={black} link='' h3='Project' p='Description'/>
-            <ProjectCard src={black} link='' h3='Project' p='Description'/>
-            <ProjectCard src={black} link='' h3='Project' p='Description'/>
+            <ProjectCard src={black} link='https://github.com/mateuszsiwy/CodingTracker' h3='Coding Tracker' p='Console application that allows users to track their daily coding time'/>
+            <ProjectCard src={black} link='https://github.com/mateuszsiwy/ShiftsLoggerApi/tree/master' h3='Shifts Logger' p='Shifts Logger is a console-based application for recording and managing worker shifts'/>
+            <ProjectCard src={black} link='https://github.com/mateuszsiwy/Bench-Press-Tracker' h3='Bench Press Tracker' p='Bench Press Tracker is a simple console-based application for tracking bench press exercises.'/>
+            <ProjectCard src={black} link='https://github.com/mateuszsiwy/Flashcards' h3='Flashcards' p='Flashcards application allows users to create and manage flashcards organized in stacks, study them, and track their progress through study sessions.'/>
         </div>
         <br/><br/>
         <h2>Fullstack Apps</h2>
         <div className={styles.projects}>
-            <ProjectCard src={black} link='' h3='Project' p='Description'/>
-            <ProjectCard src={black} link='' h3='Project' p='Description'/>
-            <ProjectCard src={black} link='' h3='Project' p='Description'/>
-            <ProjectCard src={black} link='' h3='Project' p='Description'/>
+            <ProjectCard src={black} link='https://github.com/mateuszsiwy/Chat-Application' h3='Chat Application' p='Chat Application is a .NET desktop application built using Windows Forms that allows users to communicate in real time over a local server.'/>
+
         </div>
         <br/><br/>
         <h2>Frontend Apps</h2>
         <div className={styles.projects}>
-            <ProjectCard src={black} link='' h3='Project' p='Description'/>
-            <ProjectCard src={black} link='' h3='Project' p='Description'/>
-            <ProjectCard src={black} link='' h3='Project' p='Description'/>
-            <ProjectCard src={black} link='' h3='Project' p='Description'/>
+            <ProjectCard src={black} link='https://github.com/mateuszsiwy/msiwy-portfolio' h3='Portfolio' p='The personal portfolio you are looking at.'/>
         </div>
     </section>
   )
--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -13,27 +13,25 @@ function Skills() {
     <section id='skills' className={styles.container}>
         <h1 className='sectionTitle'>Skills</h1>
         <div className={styles.skillList}>
-            <SkillList src={checkMarkIcon} skill='.Net'/>
+            <SkillList src={checkMarkIcon} skill='C#'/>
             <SkillList src={checkMarkIcon} skill='C++'/>
             <SkillList src={checkMarkIcon} skill='C'/>
-            <SkillList src={checkMarkIcon} skill='HTML'/>
-            <SkillList src={checkMarkIcon} skill='CSS'/>
+            <SkillList src={checkMarkIcon} skill='Python'/>
         </div>
         <hr/>
         <div className={styles.skillList}>
             <SkillList src={checkMarkIcon} skill='.Net'/>
-            <SkillList src={checkMarkIcon} skill='C++'/>
-            <SkillList src={checkMarkIcon} skill='C'/>
             <SkillList src={checkMarkIcon} skill='HTML'/>
             <SkillList src={checkMarkIcon} skill='CSS'/>
+            <SkillList src={checkMarkIcon} skill='Flask'/>
         </div>
         <hr/>
         <div className={styles.skillList}>
-            <SkillList src={checkMarkIcon} skill='.Net'/>
-            <SkillList src={checkMarkIcon} skill='C++'/>
-            <SkillList src={checkMarkIcon} skill='C'/>
-            <SkillList src={checkMarkIcon} skill='HTML'/>
-            <SkillList src={checkMarkIcon} skill='CSS'/>
+            <SkillList src={checkMarkIcon} skill='SQL'/>
+            <SkillList src={checkMarkIcon} skill='SQLServer'/>
+            <SkillList src={checkMarkIcon} skill='Entiry Framework Core'/>
+            <SkillList src={checkMarkIcon} skill='Visual Studio'/>
+            <SkillList src={checkMarkIcon} skill='Visual Studio Code'/>
         </div>
     </section>
   )
